27911232cc84c88f512e58925a1dec1c2e2ef21d
Sinevia <info@sinevia.com>
2019-01-23 06:09:17 +0000

    Update JsonDb.php

--- src/JsonDb.php
+++ src/JsonDb.php
@@ -19,24 +19,36 @@ class JsonDb {
     public function delete() {
         return unlink($this->filePath);
     }
+    
     /**
-     * Checkes is database exsts
+     * Checks if database exsts
      */
     public function exists() {
         return file_exists($this->filePath);
     }
+    
     /**
      * Gets a key from database
      */
     public function get($key) {
         return isset($this->data[$key]) ? $this->data[$key] : null;
     }
+    
+    /**
+     * Gets a key from database
+     */
+    public function has($key) {
+        return isset($this->data[$key]) ? true : false;
+    }
+
+    
     /**
      * Saves the data to the file
      */
     public function save() {
         return file_put_contents($this->filePath, json_encode($this->data));
     }
+    
     /**
      * Opens the database and reads data in
      */
@@ -46,6 +58,7 @@ class JsonDb {
         }
         $this->data = json_decode(file_get_contents($this->filePath), true);
     }
+    
     /**
      * Removes a key from database
      */
